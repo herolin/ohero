4b84f90ec96b58a437b86bd59e9bc016b3228fcb
herolin <hero@bossinfo.com.tw>
2026-08-07 00:59:23 +0000

    sync: g005 slower easy/medium, g006 rooftop theme

---
+++ games/g005-pacman/assets/auth-DeIaBX8H.js
@@ -0,0 +1,1 @@
+import"./index-97SD2bj6.js";const n={};function t(){return Object.keys(n).length>0}async function r(){return t(),!1}export{n as AUTH_CONFIG,t as isAuthConfigured,r as signInWithGoogle};
