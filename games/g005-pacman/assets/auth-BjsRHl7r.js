9823bb269783de5c0a94fd6c16d9792adde3c700
herolin <hero@bossinfo.com.tw>
2026-08-12 07:12:34 +0000

    sync: ad placements across all six builds (inert until configured)

---
+++ games/g005-pacman/assets/auth-BjsRHl7r.js
@@ -0,0 +1,1 @@
+import"./index-DGNbu9D1.js";const n={};function t(){return Object.keys(n).length>0}async function r(){return t(),!1}export{n as AUTH_CONFIG,t as isAuthConfigured,r as signInWithGoogle};
